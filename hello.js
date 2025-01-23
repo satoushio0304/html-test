a025a5674cdd0e342e563189839d6f8d13284b71
satoushio0304 <satoushio0304@gmail.com>
2025-01-23 13:15:43 +0900

    10デクリメント

--- hello.js
+++ hello.js
@@ -3,6 +3,10 @@ function incrementCount() {
     count++;
     document.getElementById("count").innerText = count;
 }
+function decrementCount() {
+    count--;
+    document.getElementById("count").innerText = count;
+}
 
 function navigateTo(url) {
     window.location.href = url;
