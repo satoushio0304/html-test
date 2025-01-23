fbcb4b02d7ddb9815f1cb8eadefc51e8fb57999d
satoushio0304 <satoushio0304@gmail.com>
2025-01-23 18:05:41 +0900

    18ホーム画面

---
+++ home.js
@@ -0,0 +1,7 @@
+document.getElementById('title').addEventListener('click', () => {
+    window.location.href = 'title.html';
+});
+
+document.getElementById('button').addEventListener('click', () => {
+    window.location.href = 'button.html';
+});
