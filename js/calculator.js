b79d59bee0ba5cdc5215c9a497747b7c9f05ad38
satoushio0304 <satoushio0304@gmail.com>
2025-01-24 18:33:29 +0900

    59イベントリスナー並び替え

--- js/calculator.js
+++ js/calculator.js
@@ -1,103 +1,123 @@
-let num = "";
+let str1 = "";
+let str2 = "";
+let str3 = "";
 
 document.getElementById('index').addEventListener('click', () => {
     window.location.href = 'index.html';
 });
 
-document.getElementById('lef-par').addEventListener('click', () => {
-    num = num + "(";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('rig-par').addEventListener('click', () => {
-    num = num + ")";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('cle').addEventListener('click', () => {
-    num = "";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('all-cle').addEventListener('click', () => {
-    num = "";
-    document.getElementById("num").innerText = num;
+document.getElementById('0').addEventListener('click', () => {
+    str1 = str1 + "0";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('1').addEventListener('click', () => {
-    num = num + "1";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + 1;
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('2').addEventListener('click', () => {
-    num = num + "2";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "2";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('3').addEventListener('click', () => {
-    num = num + "3";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "3";
+    document.getElementById("str1").innerText = str1;
 });
 
-document.getElementById('div').addEventListener('click', () => {
-    num = num + "/";
-    document.getElementById("num").innerText = num;
-});
 document.getElementById('4').addEventListener('click', () => {
-    num = num + "4";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "4";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('5').addEventListener('click', () => {
-    num = num + "5";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "5";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('6').addEventListener('click', () => {
-    num = num + "6";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('mul').addEventListener('click', () => {
-    num = num + "*";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "6";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('7').addEventListener('click', () => {
-    num = num + "7";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "7";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('8').addEventListener('click', () => {
-    num = num + "8";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "8";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('9').addEventListener('click', () => {
-    num = num + "9";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + "9";
+    document.getElementById("str1").innerText = str1;
 });
 
-document.getElementById('sub').addEventListener('click', () => {
-    num = num + "-";
-    document.getElementById("num").innerText = num;
-});
 document.getElementById('dec').addEventListener('click', () => {
-    num = num + ".";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('0').addEventListener('click', () => {
-    num = num + "0";
-    document.getElementById("num").innerText = num;
-});
-
-document.getElementById('equ').addEventListener('click', () => {
-    num = num + "=";
-    document.getElementById("num").innerText = num;
+    str1 = str1 + ".";
+    document.getElementById("str1").innerText = str1;
 });
 
 document.getElementById('add').addEventListener('click', () => {
-    num = num + "+";
-    document.getElementById("num").innerText = num;
+    str3 = str1;
+    str2 = "+";
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+    document.getElementById("str2").innerText = str2;
+    document.getElementById("str3").innerText = str3;
+});
+
+document.getElementById('sub').addEventListener('click', () => {
+    str3 = str1;
+    str2 = "-";
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+    document.getElementById("str2").innerText = str2;
+    document.getElementById("str3").innerText = str3;
+});
+
+document.getElementById('mul').addEventListener('click', () => {
+    str3 = str1;
+    str2 = "*";
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+    document.getElementById("str2").innerText = str2;
+    document.getElementById("str3").innerText = str3;
+});
+
+document.getElementById('div').addEventListener('click', () => {
+    str3 = str1;
+    str2 = "/";
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+    document.getElementById("str2").innerText = str2;
+    document.getElementById("str3").innerText = str3;
+});
+
+document.getElementById('lef-par').addEventListener('click', () => {
+    str1 = str1 + "(";
+    document.getElementById("str1").innerText = str1;
+});
+
+document.getElementById('rig-par').addEventListener('click', () => {
+    str1 = str1 + ")";
+    document.getElementById("str1").innerText = str1;
+});
+
+document.getElementById('cle').addEventListener('click', () => {
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+});
+
+document.getElementById('all-cle').addEventListener('click', () => {
+    str1 = "";
+    document.getElementById("str1").innerText = str1;
+});
+
+document.getElementById('equ').addEventListener('click', () => {
+    str1 = str1 + "=";
+    document.getElementById("str1").innerText = str1;
 });
