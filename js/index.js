d1d10ff489163bb01b92ff46d3e3ea3099716dfd
satoushio0304 <satoushio0304@gmail.com>
2025-01-28 17:46:21 +0900

    66ファイル名変更

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ document.getElementById('calculator').addEventListener('click', () => {
     window.location.href = 'calculator.html';
 });
 
-document.getElementById('money-management').addEventListener('click', () => {
-    window.location.href = 'money-management.html';
+document.getElementById('money').addEventListener('click', () => {
+    window.location.href = 'money.html';
 });
