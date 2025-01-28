aec0bef18f0f6e5e09138f1b476d146f3146dd8f
satoushio0304 <satoushio0304@gmail.com>
2025-01-28 20:21:55 +0900

    71画面追加

---
+++ js/calculator2.js
@@ -0,0 +1,7 @@
+document.getElementById('index').addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+const buttons = document.querySelectorAll('.button');
+const display = document.getElementById('display');
+
--- js/index.js
+++ js/index.js
@@ -5,3 +5,7 @@ document.getElementById('calculator').addEventListener('click', () => {
 document.getElementById('money').addEventListener('click', () => {
     window.location.href = 'money.html';
 });
+
+document.getElementById('calculator2').addEventListener('click', () => {
+    window.location.href = 'calculator2.html';
+});
