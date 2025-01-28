dd5f367951e8526b8820628a3f667b1e555ae5c6
satoushio0304 <satoushio0304@gmail.com>
2025-01-28 17:28:27 +0900

    64データフォルダ削除

--- js/money-management.js
+++ js/money-management.js
@@ -1,3 +1,26 @@
 document.getElementById('index').addEventListener('click', () => {
     window.location.href = 'index.html';
 });
+
+
+async function loadJSON() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+
+        const tableBody = document.getElementById('data');
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.name}</td>
+                <td>${item.price}</td>
+            `;
+            tableBody.appendChild(row);
+        })
+    } catch (error) {
+        console.error('DOMContentLoaded', loadJSON);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadJSON)
