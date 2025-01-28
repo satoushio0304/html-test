d254f4aa9ce58312a1455a8619902dee008ac58c
satoushio0304 <satoushio0304@gmail.com>
2025-01-29 02:42:11 +0900

    78四則演算

--- js/calculator2.js
+++ js/calculator2.js
@@ -1,7 +1,48 @@
 let display = "";
 
+const result2 = x => {
+    x += ",";
+    let a = "";
+    let b = "*";
+    let c = 1;    
+    for (const i of x) {
+        if (i == '*' || i == '/' || i == ','){
+            if (b == '*'){
+                c *= a;
+                a = "";
+            } else if (b == '/') {
+                c /= a;
+                a = "";
+            }
+            b = i;
+        } else {
+            a += i;
+        }
+    }
+    return c;
+}
 
-
+const result = x => {
+    x += ",";
+    let a = "";
+    let b = "+";
+    let c = 0;    
+    for (const i of x) {
+        if (i == '+' || i == '-' || i == ','){
+            a = result2(a)
+            if (b == '+'){
+                c += Number(a);
+            } else if (b == '-') {
+                c -= a;
+            }
+            a = "";
+            b = i;
+        } else {
+            a += i;
+        }
+    }
+    return c;
+}
 
 
 document.querySelectorAll('.button').forEach(button => {
@@ -11,7 +52,9 @@ document.querySelectorAll('.button').forEach(button => {
         if (this.textContent == "C") {
             display = "";
         } else if (this.textContent == "=") {
-            display = result(display);
+            if (!['+','-','*','/'].includes(display.slice(-1))) {
+                display = result(display);
+            }
         } else {
             display += this.textContent;
         }
