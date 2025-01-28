a4a90f649a9e4644ebe08f3343911f579c2d0f03
satoushio0304 <satoushio0304@gmail.com>
2025-01-28 17:49:04 +0900

    67データスクリプトを作成

---
+++ js/data.js
@@ -0,0 +1,5 @@
+const data = [
+    { id:1, name:"ハンコ", price:380 },
+    { id:2, name:"病院", price:2690 },
+    { id:3, name:"薬局", price:1490 }
+];
--- js/money.js
+++ js/money.js
@@ -2,12 +2,6 @@ document.getElementById('index').addEventListener('click', () => {
     window.location.href = 'index.html';
 });
 
-const data = [
-    { id:1, name:"ハンコ", price:380 },
-    { id:2, name:"病院", price:2690 },
-    { id:3, name:"薬局", price:1490 }
-];
-
 function displayData(data) {
     const tableBody = document.getElementById('data')
     data.forEach(item => {
