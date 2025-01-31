74334558ead9adc9637c8f01997156388347bae0
satoushio0304 <satoushio0304@gmail.com>
2025-01-31 15:10:35 +0900

    calculator.js を更新

--- js/calculator.js
+++ js/calculator.js
@@ -1,115 +1,64 @@
-const buttons = document.querySelectorAll('.button');
-const display = document.getElementById('formula');
+let display = "";
 
-
-let formula = "";
-let result = "";
-let num = "";
-let ope = "";
-
-buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        formula += this.textContent;
-        display.textContent = formula;
-
-        if (isOpe(this.textContent) == 1) {
-            if (ope == "+") {
-                result = add(result, num);
-                
-            } else if (ope == "-") {
-                result = sub(result, num);
-                
-            } else {
-                result = num;
+const result2 = x => {
+    x += ",";
+    let a = "";
+    let b = "*";
+    let c = 1;    
+    for (const i of x) {
+        if (i == '*' || i == '/' || i == ','){
+            if (b == '*'){
+                c *= a;
+                a = "";
+            } else if (b == '/') {
+                c /= a;
+                a = "";
             }
-            num = "";
-            ope = this.textContent;
-
-        } else if (this.textContent == "=") {
-            if (ope == "+") {
-                result = add(result, num);
-                
-            } else if (ope == "-") {
-                result = sub(result, num);
-                
-            } else {
-                result = num;
-            }
-            
-            display.textContent = result;
-            formula = result;
-            num = result;
-            ope = this.textContent;
-            
-        } else if (this.textContent == "C") {
-            display.textContent = 0;
-            formula = 0;
-            num = 0;
-            ope = 0;
-
-        }else {
-            num += this.textContent;
+            b = i;
+        } else {
+            a += i;
         }
+    }
+    return c;
+}
+
+const result = x => {
+    x += ",";
+    let a = "";
+    let b = "+";
+    let c = 0;    
+    for (const i of x) {
+        if (i == '+' || i == '-' || i == ','){
+            a = result2(a)
+            if (b == '+'){
+                c += Number(a);
+            } else if (b == '-') {
+                c -= a;
+            }
+            a = "";
+            b = i;
+        } else {
+            a += i;
+        }
+    }
+    return c;
+}
+
+
+document.querySelectorAll('.button').forEach(button => {
+
+    button.addEventListener('click', function() {
+
+        if (this.textContent == "C") {
+            display = "";
+        } else if (this.textContent == "=") {
+            if (!['+','-','*','/'].includes(display.slice(-1))) {
+                display = result(display);
+            }
+        } else {
+            display += this.textContent;
+        }
+        document.getElementById('display').textContent = display;
     });
+    
 });
-
-
-
-
-
-function add(a,b) {
-    return Number(a)+Number(b);
-}
-
-function sub(a,b) {
-    return a-b;
-}
-
-function mul(a,b) {
-    return a*b;
-}
-
-function div(a,b) {
-    return a/b;
-}
-
-
-function isPri1(a) {
-    if (a == "*") {
-        return 1;
-    } else if (a == "/") {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-function isPri2(a) {
-    if (a == "+") {
-        return 1;
-    } else if (a == "-") {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-function isOpe(a) {
-    if (isPri1(a) == 1) {
-        return 1;
-    } else if (isPri2(a) == 1) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-function isSym(a) {
-    if (isOpe(a) == 1) {
-        return 1;
-    } else if (a == "=") {
-        return 1;
-    } else {
-        return 0;
-    }
-}
