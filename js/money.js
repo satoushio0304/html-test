c646bd2085c1f0906f0fc80937478f0aa4e9f19b
satoushio0304 <satoushio0304@gmail.com>
2025-01-31 19:06:46 +0900

    money.js を更新

--- js/money.js
+++ js/money.js
@@ -9,6 +9,6 @@ var data = JSON.parse(data);
 Object.keys(data).forEach(function(i) {
     var li = document.createElement('li');
     li.textContent = data[i].name;
-            document.getElementById('display').appendChild();
-
+    document.getElementById('display').appendChild();
+}
 
