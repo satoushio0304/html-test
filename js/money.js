e30c9c74c931fc5fa3752ea158e2daf62653176c
satoushio0304 <satoushio0304@gmail.com>
2025-01-31 19:55:51 +0900

    todo書き込み

--- js/money.js
+++ js/money.js
@@ -1,9 +1,12 @@
+
+// (1) Use data/data.json
+// (2) Road .json file
+// (3) Use roaded .json file
 var data = [
     { id:1, name:"ハンコ", price:380 },
     { id:2, name:"病院", price:2690 },
     { id:3, name:"薬局", price:1490 }
 ];
-
 
 var sum = 0;
 
