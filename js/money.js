997ede7a6d4bba35ef93151337f6efaadaed33d8
satoushio0304 <satoushio0304@gmail.com>
2025-01-31 18:55:07 +0900

    money.js を更新

--- js/money.js
+++ js/money.js
@@ -1,7 +1,9 @@
-const data = [
+var data = [
     { id:1, name:"ハンコ", price:380 },
     { id:2, name:"病院", price:2690 },
     { id:3, name:"薬局", price:1490 }
 ];
+
+var data = JSON.parse(json);
 
 document.getElementById('display') = document.createTextNode(data[i].price)
