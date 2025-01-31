5a306dcdf5973ab2bebf4db449bd1985e1e8a196
satoushio0304 <satoushio0304@gmail.com>
2025-01-31 19:09:46 +0900

    money.js を更新

--- js/money.js
+++ js/money.js
@@ -4,6 +4,17 @@ var data = [
     { id:3, name:"薬局", price:1490 }
 ];
 
+// 親要素を指定する
+var list = document.getElementById('list');
+
+// 追加する要素を作成
+var li = document.createElement('li');
+li.innerHTML = 'a';
+
+// 末尾に追加
+list.appendChild(li);
+
+/*
 var data = JSON.parse(data);
 
 Object.keys(data).forEach(function(i) {
@@ -11,4 +22,4 @@ Object.keys(data).forEach(function(i) {
     li.textContent = data[i].name;
     document.getElementById('display').appendChild();
 }
-
+*/
