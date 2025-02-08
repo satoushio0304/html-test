3e7a2bca764a49b46d2ee6bda501ad1cfcb1a5da
satoushio0304 <satoushio0304@gmail.com>
2025-02-08 17:23:24 +0900

    hobby.pyの整理

--- hobby.py
+++ hobby.py
@@ -1,23 +1,18 @@
 import random
 import json
 
-def load(path):
-    with open(path, encoding='utf-8') as f:
-        return f.read()
-
-def tolist(data):
-    return [line.split(',') for line in data.split('\n')]
-
-
-print(tolist(load('data/data.csv')))
+def random_choice(json_path):
+    l = []
+    with open(json_path, encoding='utf-8') as f:
+        for item in json.load(f):
+            for _ in range(int(item['weight'])):
+                l.append(item['name'])
+    return random.choice(l)
 
 
-def random_choice():
-    with open('data/hobby.json', encoding='utf-8') as f:
-        print(random.choice([item['name'] for item in json.load(f) for _ in range(int(item['weight']))]))
+json_path = 'data/hobby.json'
 
-
-
+print(random_choice(json_path))
     
 
 
