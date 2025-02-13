b4f56281dc6cc94a59ac361ad5eda8513a85afdb
satoushio0304 <satoushio0304@gmail.com>
2025-02-13 14:42:57 +0900

    電卓js統合と削除

--- js/script.js
+++ js/script.js
@@ -67,6 +67,105 @@ function money(data) {
     document.getElementById('sum').textContent = sum;
 }
 
+const result2 = x => {
+    x += ",";
+    let a = "";
+    let b = "*";
+    let c = 1;    
+    for (const i of x) {
+        if (i == '*' || i == '/' || i == ','){
+            if (b == '*'){
+                c *= a;
+                a = "";
+            } else if (b == '/') {
+                c /= a;
+                a = "";
+            }
+            b = i;
+        } else {
+            a += i;
+        }
+    }
+    return c;
+}
+
+const result = x => {
+    x += ",";
+    let a = "";
+    let b = "+";
+    let c = 0;
+    for (const i of x) {
+        if (['0','1','2','3','4','5','6','7','8','9'].includes(a.slice(-1))) {
+            if (i == '+' || i == '-' || i == ','){
+                a = result2(a)
+                if (b == '+'){
+                    c += Number(a);
+                } else if (b == '-') {
+                    c -= a;
+                }
+                a = "";
+                b = i;
+            } else {
+                a += i;
+            }       
+        } else {
+            a += i;
+        }
+    }
+    return c;
+}
+
+let formula = "";
+let point = 1;
+
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function() {
+        if (["C"].includes(this.textContent)) {
+            formula =  "";
+            point = 1;
+        } else if (["←"].includes(this.textContent)) {
+            if (['.'].includes(formula.slice(0,-1))) { 
+                point = 1;
+            }
+            formula = formula.slice(0,-1);
+        } else if (["="].includes(this.textContent)) {
+            if (!['+','-','*','/','.'].includes(formula.slice(-1))) {
+                formula = String(result(formula));
+                if (Number.isInteger(Number(formula))) {
+                    point = 1;
+                }
+            }
+        } else if (["."].includes(this.textContent)) {
+            if (['0','1','2','3','4','5','6','7','8','9'].includes(formula.slice(-1))) {
+                if (point) {
+                    formula += this.textContent;
+                    point = 0;
+                }
+            }
+        } else if (['+','*','/'].includes(this.textContent)) {
+            if (['0','1','2','3','4','5','6','7','8','9'].includes(formula.slice(-1))) {
+                formula += this.textContent;
+                point = 1;
+            }
+        } else if (['-'].includes(this.textContent)) {
+            if (['0','1','2','3','4','5','6','7','8','9','*','/'].includes(formula.slice(-1))) {
+                formula += this.textContent;
+                point = 1
+            } else if (formula == '') {
+                formula += this.textContent;
+                point = 1;
+            }
+        } else if (['0','1','2','3','4','5','6','7','8','9'].includes(this.textContent)) {
+            if (!['0'].includes(formula.slice(-1))) {
+                formula += this.textContent;
+            }
+        } else {
+            formula += this.textContent;
+        }
+        document.getElementById('formula').textContent = formula;
+    }); 
+});
+
 // if class = title , id to link
 document.querySelectorAll('.title').forEach(title => {
     title.addEventListener('click', () => {
@@ -82,11 +181,6 @@ document.getElementById('input').addEventListener('change', e => {
     const reader = new FileReader();
     reader.readAsText(e.target.files[0]); 
     reader.onload = e => {
-        // 入力したファイルの文字列をそのまま出力
-        // document.getElementById('output').innerHTML = e.target.result;
-        // 入力したファイルの文字列を配列にして出力
-        // document.getElementById('output').innerHTML = txtToList(e.target.result);
-        // 入力した文字列を箇条書きで出力
         money(txtToList(e.target.result));
     }
 });
