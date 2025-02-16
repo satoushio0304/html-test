ac3beda2f3f9151c2dac954af27477b2b69c1087
satoushio0304 <satoushio0304@gmail.com>
2025-02-16 22:23:23 +0900

    電卓インライン化

--- js/script.js
+++ js/script.js
@@ -1,7 +1,113 @@
-// str to list
+const calculate = (formula) => {
+    formula.forEach((v, i) => {
+        if (Array.isArray(v)) formula[i] = calculate(v);
+    });
+    if (formula[1] === '+') formula = Number(formula[0]) + Number(formula[2]);
+    if (formula[1] === '-') formula = Number(formula[0]) - Number(formula[2]);
+    if (formula[1] === '*') formula = Number(formula[0]) * Number(formula[2]);
+    if (formula[1] === '/') formula = Number(formula[0]) / Number(formula[2]);
+    return formula;
+}
+let formula = [];
+
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', () => {
+        if ('C'.includes(button.textContent)) {
+            formula = [];
+        } else if ('←'.includes(button.textContent)) {
+            now = formula.pop().slice(0, -1);
+            if (now !== '') formula.push(now);
+        } else if ('='.includes(button.textContent)) {
+            formula.forEach((v, i) => {
+                if (v === '*' || v === '/') {
+                    formula[i + 1] = [formula[i - 1], formula[i], formula[i + 1]];
+                    formula[i - 1] = '';
+                    formula[i] = '';
+                }
+            });
+            formula = formula.filter(v => v !== '');
+            formula.forEach((v, i) => {
+                if (v === '+' || v === '-') {
+                    formula[i + 1] = [formula[i - 1], formula[i], formula[i + 1]];
+                    formula[i - 1] = '';
+                    formula[i] = '';
+                }
+            });
+            formula = formula.filter(v => v !== '');
+            formula = calculate(formula);
+            console.log(formula);
+        } else if ('0123456789'.includes(button.textContent)) {
+            if (formula.length) {
+                now = formula.pop();
+                if (now == '0') {
+                    formula.push(now);
+                } else if (now == '-') {
+                    if (formula.length) {
+                        old = formula.pop();
+                        if (isNaN(old)) {
+                            formula.push(old);
+                            formula.push(now + button.textContent);
+                        } else {
+                            formula.push(old);
+                            formula.push(now);
+                            formula.push(button.textContent);
+                        }
+                    } else {
+                        formula.push(now + button.textContent);
+                    }
+                } else if (isNaN(now)) {
+                    formula.push(now);
+                    formula.push(button.textContent);
+                } else {
+                    formula.push(now + button.textContent);
+                }
+            } else {
+                formula.push(button.textContent);
+            }
+        } else if ('.'.includes(button.textContent)) {
+            if (formula.length) {
+                now = formula.pop();
+                if (Number.isInteger(Number(now))) {
+                    formula.push(now + button.textContent);
+                } else {
+                    formula.push(now);
+                }
+            }
+        } else if ('-'.includes(button.textContent)) {
+            if (formula.length) {
+                now = formula.pop();
+                if (now.slice(-1) == '.') {
+                    formula.push(now);
+                } else if (now == '-') {
+                    formula.push(now);
+                } else {
+                    formula.push(now);
+                    formula.push(button.textContent);
+                }
+            } else {
+                formula.push(button.textContent);
+            }
+        } else if ('+*/'.includes(button.textContent)) {
+            if (formula.length) {
+                now = formula.pop();
+                if (now.slice(-1) == '.') {
+                    formula.push(now);
+                } else if (isNaN(now)) {
+                    formula.push(now);
+                } else {
+                    formula.push(now);
+                    formula.push(button.textContent);
+                }
+            }
+        }
+        document.getElementById('formula').textContent = formula.join('');
+    });
+});
+
+
 const txtToList = (txtData) => {
-    let key = '',keys = [], ele = '', eles = [], eless = [], cond = 1;
-    for (let i of txtData ) {
+    let key = '', keys = [], ele = '', eles = [], eless = [], cond = 1;
+    for (let i of txtData) {
         switch (cond) {
             case 1: // key入力中
                 if ([','].includes(i)) {
@@ -30,9 +136,8 @@ const txtToList = (txtData) => {
     }
     console.log(keys);
     console.log(eless);
-    return [keys,eless];
+    return [keys, eless];
 }
-
 
 const makeLi = (data, i, j) => {
     let li = document.createElement('li');
@@ -42,7 +147,6 @@ const makeLi = (data, i, j) => {
 
 const makeSum = (data) => data.reduce((previous, current) => previous + Number(current), 0);
 
-// list to html
 const money = (data) => {
     for (let j = 0; j < data[0].length; j++) {
         for (let i = 0; i < data[1][0].length; i++) {
@@ -52,133 +156,29 @@ const money = (data) => {
     document.getElementById('sum').textContent = makeSum(data[1][2]);
 }
 
-const result2 = x => {
-    let a = "", b = "*", c = 1;
-    x += ",";
-    for (const i of x) {
-        if ('*/,'.includes(i)){
-            c = b === '*' ? c * a : c / a;
-            a = "";
-            b = i;
-        } else {
-            a += i;
-        }
-    }
-    return c;
-}
-
-const result = x => {
-    let a = "", b = "+", c = 0;
-    x += ",";
-    for (const i of x) {
-        if (/[\d.]/.test(num.slice(-1))) {
-            if ('+-,'.includes(i)){
-                a = result2(a)
-                c = b === '+' ? c + Number(a) : c - a;
-                a = "";
-                b = i;
-            } else {
-                a += i;
-            }       
-        } else {
-            a += i;
-        }
-    }
-    return c;
-}
-
-class Calculator {
+class Money {
     constructor() {
-        this.formula = "";
-        this.point = 1;
-        this.init();
-    }
-
-    init() {
-        document.querySelectorAll('.button').forEach(button => {
-            button.addEventListener('click', () => this.handleButton(button.textContent));
+        document.getElementById('input').addEventListener('change', e => {
+            const reader = new FileReader();
+            reader.readAsText(e.target.files[0]);
+            reader.onload = e => money(txtToList(e.target.result));
         });
     }
-
-    handleButton(input) {
-        if (input === "C") {
-            this.reset();
-        } else if (input === "←") {
-            this.backspace();
-        } else if (input === "=") {
-            this.calculate();
-        } else if (input === "." && this.canAddDecimal()) {
-            this.formula += input;
-            this.point = 0;
-        } else if ('+*/'.includes(input) && this.canAddOperator()) {
-            this.formula += input;
-            this.point = 1;
-        } else if (input === "-" && this.canAddMinus()) {
-            this.formula += input;
-            this.point = 1;
-        } else if (/\d/.test(input) && this.canAddNumber(input)) {
-            this.formula += input;
-        }
-        document.getElementById('formula').textContent = this.formula;
-    }
-
-    reset() {
-        this.formula = "";
-        this.point = 1;
-    }
-
-    backspace() {
-        if (this.formula.endsWith(".")) this.point = 1;
-        this.formula = this.formula.slice(0, -1);
-    }
-
-    calculate() {
-        if (!/[\+\-\*\/\.]$/.test(this.formula)) {
-            this.formula = String(result(this.formula));
-            if (Number.isInteger(Number(this.formula))) this.point = 1;
-        }
-    }
-
-    canAddDecimal() {
-        return /\d$/.test(this.formula) && this.point;
-    }
-
-    canAddOperator() {
-        return /\d$/.test(this.formula);
-    }
-
-    canAddMinus() {
-        return /\d|\*|\/$/.test(this.formula) || this.formula === '';
-    }
-
-    canAddNumber(input) {
-        return !(input === "0" && this.formula.endsWith("0"));
-    }
 }
 
-// クラスのインスタンス生成
-new Calculator();
+new Money();
 
-// if class = title , id to link
+document.getElementById('time').innerHTML = new Date();
 document.querySelectorAll('.title').forEach(title => {
     title.addEventListener('click', () => {
         window.location.href = title.id + '.html';
     });
 });
 
-// ページを開いた時の時刻を表示
-document.getElementById('time').innerHTML = new Date();
-
-// ファイルが入れられたら
-document.getElementById('input').addEventListener('change', e => {
-    const reader = new FileReader();
-    reader.readAsText(e.target.files[0]); 
-    reader.onload = e => money(txtToList(e.target.result));
-});
 
 document.getElementById('download').addEventListener('click', () => {
     const data = document.getElementById('sum').innerHTML;
-    const blob = new Blob([data], {type: "text/plain"});
+    const blob = new Blob([data], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
