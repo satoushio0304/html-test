3f1495d1242b792b4a5919c3285b09bd8d0b35d1
satoushio0304 <satoushio0304@gmail.com>
2025-02-17 17:39:35 +0900

    関数型風電卓

--- js/script.js
+++ js/script.js
@@ -1,61 +1,29 @@
 const calculate = (formula) => {
-    formula.forEach((v, i) => {
-        if (Array.isArray(v)) formula[i] = calculate(v);
-    });
+    formula = formula.map(v => Array.isArray(v) ? calculate(v) : v);
     if (formula[1] === '+') formula = Number(formula[0]) + Number(formula[2]);
-    if (formula[1] === '-') formula = Number(formula[0]) - Number(formula[2]);
-    if (formula[1] === '*') formula = Number(formula[0]) * Number(formula[2]);
-    if (formula[1] === '/') formula = Number(formula[0]) / Number(formula[2]);
-    return formula;
+    else if (formula[1] === '-') formula = Number(formula[0]) - Number(formula[2]);
+    else if (formula[1] === '*') formula = Number(formula[0]) * Number(formula[2]);
+    else if (formula[1] === '/') formula = Number(formula[0]) / Number(formula[2]);
+    return [String(formula)];
 }
-
+const nesting = (formula, operators) => formula.reduce((newFormula, v, i, array) => operators.includes(array[i - 1]) ? [...newFormula.slice(0, -2), [newFormula.at(-2), newFormula.at(-1), v]] : [...newFormula, v],[]);
 const isNumber = (char) => !isNaN(char) && char !== '' && (char.slice(-1) !== '.');
 const isInteger = (char) => isNumber(char) && char.indexOf('.') == -1;
 const isOperator = (char) => "+-*/".includes(char);
-
 let formula = [];
-
 document.querySelectorAll('.button').forEach(button => {
     button.addEventListener('click', () => {
-        // 入力されたボタン button.textContent
-        if (button.textContent === 'C') {
-            formula = [];
-        } else if (button.textContent === '←') {
-            now = formula.pop().slice(0, -1);
-            if (now !== '') formula.push(now);
-        } else if (button.textContent === '=') {
-            formula.forEach((v, i) => {
-                if (v === '*' || v === '/') {
-                    formula[i + 1] = [formula[i - 1], formula[i], formula[i + 1]];
-                    formula[i - 1] = '';
-                    formula[i] = '';
-                }
-            });
-            formula = formula.filter(v => v !== '');
-            formula.forEach((v, i) => {
-                if (v === '+' || v === '-') {
-                    formula[i + 1] = [formula[i - 1], formula[i], formula[i + 1]];
-                    formula[i - 1] = '';
-                    formula[i] = '';
-                }
-            });
-            formula = formula.filter(v => v !== '');
-            formula = calculate(formula);
-        } else if (isNumber(button.textContent)) {
-            if (!formula.length || (formula.length && ('+*/'.includes(formula.slice(-1)[0]))) || (formula.slice(-1)[0] === '-' && isNumber(formula.slice(-2)[0]))) formula.push(button.textContent);
-            else if (formula.slice(-1)[0] !== '0' || (formula.slice(-1)[0] === '-' && '+*/'.includes(formula.slice(-2)[0]))) formula.push(formula.pop() + button.textContent);
-        } else if (button.textContent === '.' && formula.length && isInteger(formula.slice(-1)[0])) {
-            formula.push(formula.pop() + button.textContent);
-        } else if (button.textContent === '-' && (!formula.length || '+*/'.includes(formula.slice(-1)))) {
-            formula.push(button.textContent);
-        } else if (isOperator(button.textContent) && isNumber(formula.slice(-1)[0])) {
-            formula.push(button.textContent);
-        }
+        if (button.textContent === 'C') formula = [];
+        else if (button.textContent === '←' && formula.length) formula = formula.at(-1).length === 1 ? [...formula.slice(0, -1)] : [...formula.slice(0, -1), formula.at(-1).slice(0, -1)];
+        else if (button.textContent === '=') formula = calculate(nesting(nesting(formula, '/*'), '+-'));
+        else if (isNumber(button.textContent)) formula = !formula.length || (isOperator(formula.at(-1)) && isNumber(formula.at(-2))) ? [...formula, button.textContent] : formula.at(-1) !== '0' ? [...formula.slice(0, -1), formula.at(-1) + button.textContent] : [...formula];
+        else if (button.textContent === '.' && isInteger(formula.at(-1))) formula = [...formula.slice(0, -1), formula.at(-1) + button.textContent];
+        else if (button.textContent === '-' && (!formula.length || '+/*'.includes(formula.slice(-1)))) formula = [...formula, button.textContent];
+        else if (isOperator(button.textContent) && isNumber(formula.at(-1))) formula = [...formula, button.textContent];
         console.log(formula);
         document.getElementById('formula').textContent = formula.join('');
     });
 });
-
 
 const txtToList = (txtData) => {
     let key = '', keys = [], ele = '', eles = [], eless = [], cond = 1;
